cfa5df1d0472b84cf5cbc15eae943ae5a1819ec6
Gustakx36 <gustavobruno12333@gmail.com>
2023-12-28 13:40:08 -0300

    Alterações node v10

--- views/static/index.js
+++ views/static/index.js
@@ -14,6 +14,28 @@ $('#contatoNavSelect, #contatoNav').on('click', () => {
     $('#mobile-menu').removeClass('mt-16')
 });
 
+$('#produtosNavSelect, #produtosNav').on('click', async () => {
+    const resposta = await $.ajax({
+        url: 'https://rla-site.onrender.com' + '/itens',
+        dataType: 'json',
+        method: 'GET'
+    });
+    var ITENS = '';
+    for(var i = 0; i < resposta.length; i++){
+        ITENS += ITEM
+        .replace('_LINKIMAGEM_', resposta[i].caminho)
+        .replace('_DESCRICAO_', resposta[i].descricao);
+    }
+    $('#conteudo-html').html('');
+    $('#conteudo-html').html(`
+    <div class="listagem p-16 justify-center grid grid-flow-row-dense gap-20 font-mono text-white text-sm text-center font-bold leading-6 bg-stripes-purple rounded-lg">
+        ${ITENS}
+    </div>
+    `);
+    $('#mobile-menu').addClass('mt-[-' + sh + 'px]')
+    $('#mobile-menu').removeClass('mt-16')
+});
+
 $('#show-itens').on('click', async () => {
     const mt = parseInt($('#mobile-menu').css('margin-top').replace(/[a-z]+/g, ''));
     if(mt > 0){
