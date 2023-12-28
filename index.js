d50a25546ab369deda803ff34cebce4d6252cacf
Gustakx36 <gustavobruno12333@gmail.com>
2023-12-28 01:07:51 -0300

    Alterações node v3

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var express = require('express')
-var app = express()
-var path = require('path')
+const pg = require('pg');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const path = require('path');
 
-app.set('view engine', 'ejs')
-
-app.use(express.static(path.join(__dirname, '/views/static')))
-
-app.get('/', (req, res) => {
-    res.render('../views/index')
+const pool = new pg.Pool({
+    connectionString : process.env.DB_URL,
+    ssl : true
 })
 
-app.listen(8080)
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/views/static')));
+
+app.get('/', (req, res) => {
+    res.render('../views/index');
+});
+
+app.get('/teste', async (req, res) => {
+    const result = await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
+});
+
+app.get('/a', (req, res) => {
+    return res.json({a : 'a'});
+});
+
+app.listen(3000);
